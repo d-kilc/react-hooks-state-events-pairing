82f54f0964dfd17471d15b2d64a460379713da64
Daniel <dkilcullen25@gmail.com>
2022-01-18 11:53:06 -0600

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import video from "../data/video.js";
 
+import Video from './Video'
+import Comments from "./Comments";
+
 function App() {
   console.log("Here's your data:", video);
 
   return (
-    <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+    <div class="App">
+      <Video
+        url={video.embedUrl}
+        title={video.title}
+        views={video.views}
+        createdAt={video.createdAt}
+        upvotes={video.upvotes}
+        downvotes={video.downvotes}/>
+      <Comments 
+        comments={video.comments}/>
     </div>
   );
 }
